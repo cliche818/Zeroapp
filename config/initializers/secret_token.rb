52a32528ebd58ec3af411fbc3444fb232d4ea4c7
jeff <jeff.chun.yinli@gmail.com>
2014-03-22 12:55:30 -0400

    initial commit for testing heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zeroapp::Application.config.secret_key_base = '8d188e947d5bd4bfd9e772b91713bc937b87896d5b1aba7cc21c4c2d803d6022e0c7071d81842a50f44f66cf8f6f01ed31783c96b89347a1756a5e4c1ba321e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zeroapp::Application.config.session_store :cookie_store, key: '_zeroapp_session'
