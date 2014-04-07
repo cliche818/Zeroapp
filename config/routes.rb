e875eb28f237ec49c324585d54e0b4c560f4f45a
jeff <jeff.chun.yinli@gmail.com>
2014-04-07 09:06:31 -0700

    removed Users scaffold while keeping the user controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Zeroapp::Application.routes.draw do
   match '/help', to: 'static_pages#help', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
 
-  resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
