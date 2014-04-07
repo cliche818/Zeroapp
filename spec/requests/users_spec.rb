05cff725b409da3fcf1516bda5ba58134f24452b
jeff <jeff.chun.yinli@gmail.com>
2014-04-07 09:43:59 -0700

    added user model

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'Users pages' do
 
-  it "should have the content 'New user'"  do
+  it "should have the content 'Sign up'"  do
     visit signup_path
-    expect(page).to have_content ('New user')
+    expect(page).to have_content ('Sign up')
   end
 
   it "should have the title 'Cliche818 | Sign up'" do
