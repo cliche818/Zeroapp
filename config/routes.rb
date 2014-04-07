036ab8d169b54dcf42e226a2736f3890fdbd0d51
jeff <jeff.chun.yinli@gmail.com>
2014-04-07 04:04:14 -0700

    changed routes (removed static_pages from url)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Zeroapp::Application.routes.draw do
 
+  root 'static_pages#home'
 
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+
 
   resources :microposts
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,54 +3,54 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "home page" do
     it "should have the content 'My Trophy Collection'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content ('My Trophy Collection')
     end
 
     it "should have the base title 'Cliche818'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('Cliche818')
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
+
       expect(page).not_to have_title('Cliche818| Home')
     end
   end
 
   describe "help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title 'Cliche818 | Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title('Cliche818 | Help')
     end
   end
 
   describe "About page" do
     it "should have the content 'About Me'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Me')
     end
 
     it "should have the right title 'Cliche818 | About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title('Cliche818 | About')
     end
   end
 
-
   describe "Contact page" do
     it "should have the content 'Contact Me'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact Me')
     end
 
     it "should have the right title 'Cliche818 | Contact Me'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title('Cliche818 | Contact Me')
     end
   end
