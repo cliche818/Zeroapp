b843a6073c174610b467b19061a2ac6ef48179e5
jeff <jeff.chun.yinli@gmail.com>
2014-04-07 04:46:30 -0700

    trying a gem for css fix on heroku

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module Zeroapp
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
-    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
