2282bb2f455a9c712cf35de696574c0eb0b9fd0f
jeff <jeff.chun.yinli@gmail.com>
2014-04-07 04:09:48 -0700

    refactored static_pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,55 +2,61 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "home page" do
-    it "should have the content 'My Trophy Collection'" do
+    before do
       visit root_path
+    end
+
+    it "should have the content 'My Trophy Collection'" do
       expect(page).to have_content ('My Trophy Collection')
     end
 
     it "should have the base title 'Cliche818'" do
-      visit root_path
       expect(page).to have_title('Cliche818')
     end
 
     it "should not have a custom page title" do
-      visit root_path
-
       expect(page).not_to have_title('Cliche818| Home')
     end
   end
 
   describe "help page" do
-    it "should have the content 'Help'" do
+    before do
       visit help_path
+    end
+
+    it "should have the content 'Help'" do
       expect(page).to have_content('Help')
     end
 
     it "should have the right title 'Cliche818 | Help'" do
-      visit help_path
       expect(page).to have_title('Cliche818 | Help')
     end
   end
 
   describe "About page" do
-    it "should have the content 'About Me'" do
+    before do
       visit about_path
+    end
+
+    it "should have the content 'About Me'" do
       expect(page).to have_content('About Me')
     end
 
     it "should have the right title 'Cliche818 | About'" do
-      visit about_path
       expect(page).to have_title('Cliche818 | About')
     end
   end
 
   describe "Contact page" do
-    it "should have the content 'Contact Me'" do
+    before do
       visit contact_path
+    end
+
+    it "should have the content 'Contact Me'" do
       expect(page).to have_content('Contact Me')
     end
 
     it "should have the right title 'Cliche818 | Contact Me'" do
-      visit contact_path
       expect(page).to have_title('Cliche818 | Contact Me')
     end
   end
