2ae8f8f9a7370b433df1f8eaa3489c96b9b87e45
jeff <jeff.chun.yinli@gmail.com>
2014-04-21 15:43:03 -0700

    removed rspec method for production environment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,9 +16,4 @@ module ApplicationHelper
     click_button "Sign in"
   end
 
-  RSpec::Matchers.define :have_error_message do |message|
-    match do |page|
-      expect(page).to have_selector('div.alert.alert-error', text: message)
-    end
-  end
 end
